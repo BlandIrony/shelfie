b30f5ae009ef4ae051713c2d45de2e09992b9f3c
Bland Irony <blandirony@gmail.com>
2026-01-05 09:50:40 +0100

    :sparkles: Fixed sidebar component.

--- src/app/modules/globals/components/SidebarComponent.tsx
+++ src/app/modules/globals/components/SidebarComponent.tsx
@@ -1,48 +1,49 @@
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem
-} from "@/src/app/components/ui/sidebar"
-import { items } from "@/src/app/lib/data/data"
+// import {
+//     Sidebar,
+//     SidebarContent,
+//     SidebarFooter,
+//     SidebarGroup,
+//     SidebarGroupContent,
+//     SidebarGroupLabel,
+//     SidebarHeader,
+//     SidebarMenu,
+//     SidebarMenuButton,
+//     SidebarMenuItem
+// } from "@/src/app/components/ui/sidebar"
+// import { items } from "@/src/app/lib/data/data"
 
 export default function SidebarComponent() {
     return (
-        <Sidebar>
-            <SidebarHeader>
-                Shelfie
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>
-                        Menu
-                    </SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {
-                                items.map((item) => (
-                                    <SidebarMenuItem key={item.title}>
-                                        <SidebarMenuButton asChild>
-                                            <a href={item.url}>
-                                                <item.icon className="text-[2rem]"/>
-                                                <span>
-                                                    { item.title }
-                                                </span>
-                                            </a>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))
-                            }
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
+        <div></div>
+        // <Sidebar>
+        //     <SidebarHeader>
+        //         Shelfie
+        //     </SidebarHeader>
+        //     <SidebarContent>
+        //         <SidebarGroup>
+        //             <SidebarGroupLabel>
+        //                 Menu
+        //             </SidebarGroupLabel>
+        //             <SidebarGroupContent>
+        //                 <SidebarMenu>
+        //                     {
+        //                         items.map((item) => (
+        //                             <SidebarMenuItem key={item.title}>
+        //                                 <SidebarMenuButton asChild>
+        //                                     <a href={item.url}>
+        //                                         <item.icon className="text-[2rem]"/>
+        //                                         <span>
+        //                                             { item.title }
+        //                                         </span>
+        //                                     </a>
+        //                                 </SidebarMenuButton>
+        //                             </SidebarMenuItem>
+        //                         ))
+        //                     }
+        //                 </SidebarMenu>
+        //             </SidebarGroupContent>
+        //         </SidebarGroup>
+        //     </SidebarContent>
+        // </Sidebar>
     )
 }
